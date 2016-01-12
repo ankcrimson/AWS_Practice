11681d78401aa17e9fbf026b20a001e3431daff2
Ankur Srivastava <Ankur.Srivastava@nike.com>
2016-01-11 21:00:25 -0800

    updating code

--- S3Sample/src/main/java/com/amazonaws/samples/DeleteAllS3.java
+++ S3Sample/src/main/java/com/amazonaws/samples/DeleteAllS3.java
@@ -11,6 +11,7 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.Bucket;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ListObjectsRequest;
 import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
@@ -28,6 +29,17 @@ public class DeleteAllS3 {
 		
 		List<Bucket> buckets = client.listBuckets();
 		for(Bucket bucket:buckets) {
+			
+			ListObjectsRequest listObjectsRequest = new ListObjectsRequest().withBucketName(bucket.getName());
+			ObjectListing objectListing=client.listObjects(listObjectsRequest);
+			
+			
+			for(S3ObjectSummary objectSummary : objectListing.getObjectSummaries()) {
+				System.out.println(objectSummary.getKey());
+				client.deleteObject(bucket.getName(), objectSummary.getKey());
+			}
+			
+			
 			System.out.println("Deleting "+bucket.getName());
 			client.deleteBucket(bucket.getName());
 		}
